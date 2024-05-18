2bb7c6f74b0f308cfcd614a3ac0b3e24a3e968fd
Sujan Ghimire <ghimire.amrit147@gmail.com>
2024-05-18 18:29:35 +0545

    express boilerplate with admin authentication

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const app = express()
+const morgan = require("morgan")
+const mongoose = require("mongoose")
+const connectDB = require("./config/db")
+const authRoutes = require("./routes/authRoutes")
+require("dotenv").config()
+
+//database config
+connectDB()
+
+//routes
+app.use("/api/v1/auth", authRoutes)
+
+//rest api
+app.get("/", (req, res) => {
+  res.send({
+    message: "hello",
+  })
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Server running on ${process.env.DEV_MODE} on http://localhost:${process.env.PORT}`
+  )
+})
